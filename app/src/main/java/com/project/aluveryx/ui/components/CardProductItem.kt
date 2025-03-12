92bd775ea4aef64711f52bb8b66ebe3ab2e2f521
Livia Carvalho <livia.carvalho.lb@gmail.com>
2025-03-12 08:16:59 -0400

    feat(CardProductItem): created CardProductItem component

---
+++ app/src/main/java/com/project/aluveryx/ui/components/CardProductItem.kt
@@ -0,0 +1,80 @@
+package br.com.alura.aluvery.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.project.aluveryx.model.Product
+import com.project.aluveryx.R
+import com.project.aluveryx.extensions.toBrazilianCurrency
+import com.project.aluveryx.sampleData.sampleProducts
+import com.project.aluveryx.ui.theme.AluveryXTheme
+
+@Composable
+fun CardProductItem(
+    product: Product,
+    modifier: Modifier = Modifier,
+    elevation: Dp = 4.dp
+) {
+    Card(
+        modifier = modifier.fillMaxWidth().heightIn(150.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = elevation
+        )
+    ) {
+        Column {
+            AsyncImage(
+                model = product.image,
+                contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(100.dp),
+                placeholder = painterResource(id = R.drawable.placeholder),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.onPrimary)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = product.name
+                )
+                Text(
+                    text = product.price.toBrazilianCurrency()
+                )
+            }
+             Text(
+                 text = product.description,
+                 modifier = Modifier.padding(16.dp),
+                 maxLines = 4,
+                 overflow = TextOverflow.Ellipsis
+             )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CardProductItemPreview() {
+    AluveryXTheme {
+        Surface {
+            CardProductItem(
+                product = sampleProducts.random(),
+            )
+        }
+    }
+}
