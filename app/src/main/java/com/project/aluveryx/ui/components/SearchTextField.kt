ebefd82bc79cf1711d0898ad5cf3023d545cc818
Livia Carvalho <livia.carvalho.lb@gmail.com>
2025-03-17 07:50:43 -0400

    feat(refactor): apply state hoisting to SearchTextField composable

--- app/src/main/java/com/project/aluveryx/ui/components/SearchTextField.kt
+++ app/src/main/java/com/project/aluveryx/ui/components/SearchTextField.kt
@@ -1,6 +1,5 @@
 package com.project.aluveryx.ui.components
 
-import android.util.Log
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,24 +9,21 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.project.aluveryx.ui.theme.AluveryXTheme
 
 @Composable
-fun SearchTextField(modifier: Modifier = Modifier) {
-    var value by rememberSaveable { mutableStateOf("") }
+fun SearchTextField(
+    text: String,
+    onTextChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
     OutlinedTextField(
-        value = value,
-        onValueChange = { newValue ->
-            value = newValue
-            Log.i("HomeScreen", "new value = $newValue")
-        },
+        value = text,
+        onValueChange = { newValue -> onTextChange(newValue) },
         modifier = modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp),
@@ -40,7 +36,8 @@ fun SearchTextField(modifier: Modifier = Modifier) {
         },
         leadingIcon = {
             Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
-        }
+        },
+        maxLines = 1
     )
 }
 
@@ -48,6 +45,6 @@ fun SearchTextField(modifier: Modifier = Modifier) {
 @Composable
 private fun SearchTextFieldPreview() {
     AluveryXTheme {
-        SearchTextField()
+        SearchTextField("", {})
     }
 }
